902dac27f286cfdf66e5b244e67ab206cee40956
Gerrit Meier <meistermeier@gmail.com>
2022-03-18 17:27:21 +0100

    Trivial load test.

---
+++ src/main/java/com/meistermeier/sdn/loadtest/Neo4jConfiguration.java
@@ -0,0 +1,42 @@
+package com.meistermeier.sdn.loadtest;
+
+import ac.simons.neo4j.migrations.core.MigrationVersion;
+import ac.simons.neo4j.migrations.core.Migrations;
+import ac.simons.neo4j.migrations.core.MigrationsConfig;
+import org.jetbrains.annotations.NotNull;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.AbstractNeo4jConfig;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.testcontainers.containers.Neo4jContainer;
+
+@Configuration
+@EnableNeo4jRepositories(considerNestedRepositories = true)
+public class Neo4jConfiguration extends AbstractNeo4jConfig {
+
+	@Override
+	@NotNull
+	@Bean
+	public Driver driver() {
+		return GraphDatabase.driver(neo4jContainer().getBoltUrl());
+	}
+
+	private Neo4jContainer<?> neo4jContainer() {
+		Neo4jContainer<?> neo4jContainer = new Neo4jContainer<>("neo4j:4.4")
+//				.withReuse(true)
+				.withoutAuthentication();
+		neo4jContainer.start();
+		return neo4jContainer;
+	}
+
+	@Bean
+	public MigrationVersion migrationVersion() {
+		MigrationsConfig migrationsConfig = MigrationsConfig.builder().withLocationsToScan("classpath:/neo4j").build();
+		var migrations = new Migrations(migrationsConfig, driver());
+		var migrationVersionOptional = migrations.apply();
+		return migrationVersionOptional.get();
+	}
+
+}
---
+++ src/test/java/com/meistermeier/sdn/loadtest/LoadTest.java
@@ -0,0 +1,94 @@
+package com.meistermeier.sdn.loadtest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Relationship;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = Neo4jConfiguration.class)
+class LoadTest {
+
+	@Test
+	@Timeout(10)
+	void loadLotsOfPaths(@Autowired ARepository repository) {
+		assertThat(repository.doThings().bs).hasSize(84);
+	}
+
+	@Test
+	@Timeout(10)
+	void loadLotsOfNode(@Autowired NodeRepository repository) {
+		assertThat(repository.findAll()).hasSize(300_000);
+	}
+
+	interface ARepository extends Neo4jRepository<A, Long> {
+
+		@Query("""
+				MATCH aPath = (a:A)
+				MATCH bPath = (a)-[:HAS]->(b:B)-[:AS|:BS]->(c:C)
+				OPTIONAL MATCH dPath =
+				(c)-[:CS]->(d:D)-[:CS]->(c2:C),
+				(b)-[:AS|:BS]->(c2)
+				RETURN
+				aPath, collect(nodes(bPath)), collect(relationships(bPath)),
+				collect(nodes(dPath)), collect(relationships(dPath))
+				""")
+		A doThings();
+
+	}
+
+	@org.springframework.data.neo4j.core.schema.Node("A")
+	public static class A {
+
+		@Id
+		@GeneratedValue
+		private Long id;
+		@Relationship("HAS")
+		List<B> bs;
+	}
+
+	@org.springframework.data.neo4j.core.schema.Node("B")
+	public static class B {
+		@Id @GeneratedValue private Long id;
+		@Relationship("AS")
+		List<C> as;
+		@Relationship("BS")
+		List<C> bs;
+	}
+
+	@org.springframework.data.neo4j.core.schema.Node("C")
+	public static class C {
+		@Id @GeneratedValue private Long id;
+		@Relationship("CS")
+		List<D> cs;
+	}
+
+	@org.springframework.data.neo4j.core.schema.Node("D")
+	public static class D {
+		@Id @GeneratedValue private Long id;
+
+		@Relationship("CS")
+		List<C> cs;
+	}
+
+	interface NodeRepository extends Neo4jRepository<Node, Long> {}
+
+	@org.springframework.data.neo4j.core.schema.Node("Blubb")
+	public static class Node {
+		@Id @GeneratedValue public Long id;
+		String value;
+		String value2;
+	}
+
+}
